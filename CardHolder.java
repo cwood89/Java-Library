6fb60ffddd06072a43ec00cf4d96e17dae005311
cwood89 <cwood89@mac.com>
2020-05-20 20:11:49 -0400

    updated cardholder class

--- CardHolder.java
+++ CardHolder.java
@@ -7,4 +7,12 @@ public class CardHolder {
   CardHolder(String Name) {
     this.name = name;
   }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
 }
